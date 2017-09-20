f7ca006cc0ca6735211a978aab9a65d6bde1f1ff
JonathanHerrmann <jonathan.herrmann@mobilityhouse.com>
2017-09-20 11:07:00 +0200

    assert right suit

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,6 +1,8 @@
 class Card:
 
     def __init__(self, suit):
+        if suit not in ('spade', 'club', 'diamond', 'heart'):
+            raise ValueError('This is a standart cardgame')
         self.suit = suit
 
     pass
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -1,9 +1,15 @@
 from blackjack import Card, Deck
+import pytest
 
 
 def test_card_has_suit():
     card = Card('diamond')
     assert hasattr(card, 'suit')
+
+
+def test_card_has_valid_suit():
+    with pytest.raises(ValueError):
+        Card('somestupidsuit')
 
 
 def test_deck():
