1073e92c01a01261682e1d86e5bdfb9d547ed92e
JonathanHerrmann <jonathan.herrmann@mobilityhouse.com>
2017-09-22 12:02:37 +0200

    added logic into deckclass

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,11 +1,14 @@
+import random
+
+
 class Card:
 
     def __init__(self, suit, value):
-        if suit not in ('spade', 'club', 'diamond', 'heart'):
+        if suit not in ['spade', 'club', 'diamond', 'heart']:
             raise ValueError('Unexpected suit. This is a standard Deck')
         self.suit = suit
 
-        if value not in ['A', 'K', 'Q', 'J'] and value not in list(range(2, 10)):
+        if value not in ['A', 'K', 'Q', 'J'] and value not in list(range(2, 11)):
             raise ValueError('Not a valid value')
 
         self.value = value
@@ -26,6 +29,24 @@ class Card:
     def __eq__(self, other):
         return self.numeric_value == other.numeric_value
 
+    def __repr__(self):
+        return "<{} of {}s>".format(self.value, self.suit)
+
+    def __str__(self):
+        return repr(self)
+
 
 class Deck:
-    pass
+
+    """A Standard deck class with 52 cards, 13 cards in each suit"""
+
+    def __init__(self):
+        self.cards = []  #same as list()
+
+        for suit in ['spade', 'club', 'diamond', 'heart']:
+            for value in list(range(2, 11)) + ['J', 'Q', 'K', 'A']:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -35,5 +35,31 @@ def test_greater_than():
     assert True == (Card('spade', 'A') > Card('club', 4))
 
 
-def test_deck():
+def test_init_deck():
+    deck = Deck()
+    assert len(deck.cards) == 52
+
+
+def test_shuffle_deck():
+    deck_a = Deck()
+    deck_b = Deck()
+
+    for c_a, c_b in zip(deck_a.cards, deck_b.cards):
+        assert c_a.value == c_b.value
+
+        # checks that the decks are identical
+
+    deck_b.shuffle()
+    randomness = []
+    for c_a, c_b in zip(deck_a.cards, deck_b.cards):
+        randomness.append(c_a.value != c_b.value)
+
+    assert any(randomness)
+
+
+def test_deal_hand():
     pass
+
+
+def test_proper_repr():
+    assert "<A of spades>" == repr(Card("spade", "A"))
