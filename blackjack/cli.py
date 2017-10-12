1b88e3155c96037c82b0a71757ee99a224017de1
JonathanHerrmann <jonathan.herrmann@mobilityhouse.com>
2017-10-12 11:49:50 +0200

    small fixes

--- blackjack/cli.py
+++ blackjack/cli.py
@@ -1,2 +1,21 @@
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest="cmd")
+
+    server = subparser.add_parser("serve", description="Start a blackjack game server")
+    client = subparser.add_parser("join", description="Join a blackjack server")
+
+    client.add_argument("server", type=str, help="The server to join")
+
+
 def main():
-    print ("Skeleton for the main func")
+    parser = get_parser()
+    options = parser.parse_args()
+    if not options.cmd:
+        parser.print_help()
+
+    if options.cmd == "server":
+        with BlackjackServer()
